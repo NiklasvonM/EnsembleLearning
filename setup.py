6f7f621ce4400587734cc9ca6ee994d5e7a9065a
Niklas von Moers <49690836+NiklasvonM@users.noreply.github.com>
2023-04-09 09:47:18 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="weighted-sum-opt",
+    name="ensemble_learning",
     version="0.1.0",
+    author="Niklas von Moers",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "cvxpy",
+    ],
+    extras_require={
+        "examples": [
+            "pandas",
+            "matplotlib",
+            "statsmodels",
+            "pmdarima",
+            "sktime",
+        ],
+    },
 )
