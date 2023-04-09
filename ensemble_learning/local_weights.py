1e8eb8c73c830a7c279e7edf9d90aba9abaa3958
NiklasvonM <niklas.von.moers2@t-online.de>
2023-04-09 19:20:35 +0200

    fix local weights and tests

---
+++ ensemble_learning/local_weights.py
@@ -0,0 +1,65 @@
+import cvxpy as cp
+import numpy as np
+
+def local_weights(predictions, target, alpha = 0.2, beta = 1):
+    """
+    Computes the optimal weights for combining the predictions to match the target value target,
+    using linear optimization with a regularization term.
+
+    The optimization problem is formulated as follows:
+
+    Let x_i' = (x_i - min(x_1,...,x_k,y)) / (max(x_1,...,x_k,y) - min(x_1,...,x_k,y))
+    Let y' = (y - min(x_1,...,x_k,y)) / (max(x_1,...,x_k,y) - min(x_1,...,x_k,y))
+
+    min  | w_1 * x_1' + ... + w_k * x_k' - y' |
+         + lambda * (sum_i=1^k w_i * |x_i' - y'|) / (max(x_1,...,x_k,y) - min(x_1,...,x_k,y))
+
+    subject to w_i >= 0, for all i = 1, ..., k
+               sum(w_i) = 1
+
+    where x_i are the predictions, y is the target value
+    and lambda is the regularization parameter.
+
+    Args:
+        predictions (list[float]): A list of k predictions.
+        target (float): The target value.
+        alpha (float): First regularization parameter,
+        specifying 
+        beta (float): Second regularization parameter
+
+
+    Returns:
+        np.ndarray: A numpy array of the optimal weights.
+    """
+
+    k = len(predictions)
+
+    # Return uniform weights if all predictions and target coincide
+    if len(set(predictions + [target])) == 1:
+        return np.full(k, 1/k)
+
+    min_value = min(predictions + [target])
+    max_value = max(predictions + [target])
+    range_values = max_value - min_value
+    scaled_predictions = np.array([(x - min_value) / range_values for x in predictions])
+    scaled_target = (target - min_value) / range_values
+
+    weights = cp.Variable(k)
+    # Set the initial value of weights to the uniform distribution for a warm start.
+    weights.value = np.full(k, 1/k)
+
+    penalty_deviation_from_uniform = cp.sum((weights - np.full(k, 1/k))**2)
+    penalty_poor_prediction = cp.sum(cp.multiply(weights, cp.abs(scaled_predictions - np.full(k, scaled_target)))) / k
+    absolute_error = cp.abs(cp.sum(cp.multiply(weights, scaled_predictions)) - scaled_target)
+
+    objective = cp.Minimize(alpha * penalty_deviation_from_uniform + (1-alpha) * penalty_poor_prediction + beta * absolute_error)
+
+    constraints = [weights >= 0, cp.sum(weights) == 1]
+    problem = cp.Problem(objective, constraints)
+    problem.solve()
+
+    return weights.value
+
+if __name__ == "__main__":
+    w = local_weights([1, 2, 3], 2, alpha=0, beta=1)
+    print(f"Weights: {w.round(4)}")
--- test/test_local_weights.py
+++ test/test_local_weights.py
@@ -1,34 +1,42 @@
 import unittest
 import numpy as np
+from ensemble_learning.local_weights import local_weights
 
 class TestWeightedSumOptimization(unittest.TestCase):
 
     def test_scaling(self):
         predictions = [1, 3, 5, 7, 9]
-        y = 5
+        target = 5
         lambda_val = 0.1
         c = 2
-        
-        weights = weighted_sum_optimization(predictions, y, lambda_val)
-        scaled_weights = weighted_sum_optimization([x * c for x in predictions], y * c, lambda_val)
-        
+
+        weights = local_weights(predictions, target, lambda_val)
+        scaled_weights = local_weights([x * c for x in predictions], target * c, lambda_val)
+
         np.testing.assert_allclose(weights, scaled_weights, rtol=1e-5, atol=1e-8)
 
     def test_sum_to_one(self):
         predictions = [2, 4, 6, 8, 10]
-        y = 7
+        target = 7
         lambda_val = 0.1
 
-        weights = weighted_sum_optimization(predictions, y, lambda_val)
+        weights = local_weights(predictions, target, lambda_val)
         self.assertAlmostEqual(np.sum(weights), 1, delta=1e-8)
 
-    def test_known_solution(self):
-        predictions = [2, 3, 4]
-        y = 3
-        lambda_val = 0.1
+    def test_uniform_weights(self):
+        predictions = [1, 2, 3]
+        target = 2
 
-        weights = weighted_sum_optimization(predictions, y, lambda_val)
-        expected_weights = np.array([0.33333333, 0.33333333, 0.33333333])
+        weights = local_weights(predictions, target, alpha=1, beta=1)
+        expected_weights = np.full(3, 1/3)
+        np.testing.assert_allclose(weights, expected_weights, rtol=1e-5, atol=1e-8)
+
+    def test_best_weight(self):
+        predictions = [1, 2, 3]
+        target = 2
+
+        weights = local_weights(predictions, target, alpha=0, beta=1)
+        expected_weights = np.array([0, 1, 0])
         np.testing.assert_allclose(weights, expected_weights, rtol=1e-5, atol=1e-8)
 
 if __name__ == '__main__':
