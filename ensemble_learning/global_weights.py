5fdceac7a4e09e998653ca0741eff693b8b42d52
NiklasvonM <niklas.von.moers2@t-online.de>
2023-04-09 19:20:00 +0200

    add global weights and fix tests

---
+++ ensemble_learning/global_weights.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def global_weights(predictions, targets):
+    """
+    Fit a linear model without an intercept and return the weight vector.
+
+    This function takes an (n x k) matrix of predictions and a vector y of length n as input,
+    fits a linear model without an intercept, and returns the weight vector w of length k.
+
+    Parameters
+    ----------
+    predictions : array-like, shape (n, k)
+        A matrix with n rows and k columns, where each column corresponds to a vector
+        of predictions from a different model.
+
+    y : array-like, shape (n,)
+        A vector of true target values.
+
+    Returns
+    -------
+    weights : array, shape (k,)
+        The estimated weights of the linear model without an intercept.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> predictions = np.array([[1, 2], [3, 4], [5, 6]])
+    >>> y = np.array([2, 4, 6])
+    >>> weights = global_weights(predictions, y)
+    >>> print(weights)
+    [0.4, 0.8]
+    """
+    # Ensure predictions is a numpy array
+    predictions = np.array(predictions)
+
+    # Ensure y is a numpy array
+    targets = np.array(targets)
+
+    # Check if the dimensions match
+    if predictions.shape[0] != targets.shape[0]:
+        raise ValueError("The number of rows in 'predictions' should match the length of 'targets'.")
+
+    # Fit the linear model without an intercept
+    model = LinearRegression(fit_intercept=False)
+    model.fit(predictions, targets)
+
+    coefficients = np.array(model.coef_)
+    coefficients = [coefficient if coefficient > 0 else 0 for coefficient in coefficients]
+    if set(coefficients) == set([0]):
+        k = predictions.shape[1]
+        return np.full(k, 1/k)
+    weights = coefficients / np.sum(coefficients)
+
+    return weights
--- test/test_global_weights.py
+++ test/test_global_weights.py
@@ -1,24 +1,48 @@
 import unittest
 import numpy as np
-from global_weights import global_weights
+from ensemble_learning.global_weights import global_weights
 
 class TestGlobalWeights(unittest.TestCase):
-    def test_global_weights(self):
-        predictions = np.array([[1, 2], [3, 4], [5, 6]])
-        y = np.array([2, 4, 6])
+    def test_perfect_model(self):
+        random_values1 = np.random.rand(10)
+        random_values2 = np.random.rand(10)
+        predictions = np.column_stack((random_values1, random_values2))
+        targets = random_values1
 
-        expected_weights = np.array([0.4, 0.8])
+        expected_weights = np.array([1, 0])
 
-        actual_weights = global_weights(predictions, y)
+        actual_weights = global_weights(predictions, targets)
+
+        np.testing.assert_almost_equal(actual_weights, expected_weights, decimal=7)
+
+    def test_equal_predictions(self):
+        random_values1 = np.random.rand(10)
+        random_values2 = np.random.rand(10)
+        predictions = np.column_stack((random_values1, random_values1))
+        targets = random_values2
+
+        expected_weights = np.array([0.5, 0.5])
+
+        actual_weights = global_weights(predictions, targets)
 
         np.testing.assert_almost_equal(actual_weights, expected_weights, decimal=7)
 
     def test_invalid_input_shapes(self):
         predictions = np.array([[1, 2], [3, 4]])
-        y = np.array([2, 4, 6])
+        targets = np.array([2, 4, 6])
 
         with self.assertRaises(ValueError):
-            global_weights(predictions, y)
+            global_weights(predictions, targets)
+
+    def test_negative_coefficients_handling(self):
+        predictions = np.array([[1, 2]])
+        targets = [-1]
+
+        expected_weights = np.full(2, 0.5)
+
+        actual_weights = global_weights(predictions, targets)
+        np.testing.assert_almost_equal(actual_weights, expected_weights, decimal=7)
+
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
